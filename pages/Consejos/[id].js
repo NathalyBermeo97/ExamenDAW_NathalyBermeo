f6863deae8e91378261b507fa566347007753be8
Nathaly Bermeo <nathaly.bermeo@epn.edu.ec>
2021-09-21 20:43:15 -0500

    arreglo

--- pages/Consejos/[id].js
+++ pages/Consejos/[id].js
@@ -2,9 +2,9 @@ import { useRouter } from "next/router";
 
 const AdviceDetailPage = () => {
     const router = useRouter();
-    console.log('router', router.query);
+    //console.log('router', router.query);
 
     const {id} = router.query;
-    console.log('id',id);
+    //console.log('id',id);
 };
 export default AdviceDetailPage;
