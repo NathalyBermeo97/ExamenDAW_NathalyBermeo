30afdabafe5f392a53c88c6e62f863aef630d66e
Nathaly Bermeo <nathaly.bermeo@epn.edu.ec>
2021-09-21 20:58:28 -0500

    vercel1

--- pages/Consejos/index.js
+++ pages/Consejos/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import {Button} from "@mui/material";
+//import axios from 'axios';
+//import {Button} from "@mui/material";
 
 
 const AdvicePage = () => {
